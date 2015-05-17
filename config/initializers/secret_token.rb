8c963a99a215cd73c3362422720c7438f66b3834
sjkim <ksj121314@gmail.com>
2015-05-17 06:58:54 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookle::Application.config.secret_key_base = 'a900874276b84dbae88ebbdc70cbd8541f1185125ca851b79bbd89a9489fa342141a8a186cef53b87f61a1d3792f86bf72d76fde6662e18db52d3e5d8f448828'
